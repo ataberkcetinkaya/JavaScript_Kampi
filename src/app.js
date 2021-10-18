9d185fd87c82907f2adc966f14de3a29d076da42
ataberkcetinkaya <chpataberk@outlook.com.tr>
2021-10-18 23:38:06 +0300

    JS_kamp 23:38

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("berklinux s.a")
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2 //const'a bir değer atandığında sabit kalır, artık değişmez
+//euroDun = 11 //Hatali olacaktir
+
+console.log(euroDun)
+
+//array - birden fazla veri ayni yerde
+let konutKredileri =  ["Konut Kredisi", "Taşıt Kredisi", "Berk Kredisi", 12]
+
+for 
+
+console.log(konutKredileri)
